d79ccbaaf2c0edfcc0c569723578076cd8058888
zelli17 <aayushnair321@gmail.com>
2025-02-27 23:38:32 +0530

    dock links

--- frontend/src/components/BottomDock.tsx
+++ frontend/src/components/BottomDock.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import React from "react";
 import { Dock, DockIcon } from "@/components/magicui/dock";
 import { DollarSign, History, House, User } from "lucide-react";
+import Link from "next/link";
+import React from "react";
 
 export type IconProps = React.HTMLAttributes<SVGElement>;
 
@@ -10,18 +11,26 @@ export function DockDemo() {
   return (
     <div className="fixed bottom-0 left-1/2 -translate-x-1/2 mb-4">
       <Dock direction="middle">
-        <DockIcon>
-          <House className="size-6" />
-        </DockIcon>
-        <DockIcon>
-          <DollarSign className="size-6" />
-        </DockIcon>
-        <DockIcon>
-          <History className="size-6" />
-        </DockIcon>
-        <DockIcon>
-          <User className="size-6" />
-        </DockIcon>
+        <Link href="/dashboard">
+          <DockIcon>
+            <House className="size-6" />
+          </DockIcon>
+        </Link>
+        <Link href="/pay">
+          <DockIcon>
+            <DollarSign className="size-6" />
+          </DockIcon>
+        </Link>
+        <Link href="/payment-history">
+          <DockIcon>
+            <History className="size-6" />
+          </DockIcon>
+        </Link>
+        <Link href="/contacts">
+          <DockIcon>
+            <User className="size-6" />
+          </DockIcon>
+        </Link>
       </Dock>
     </div>
   );
